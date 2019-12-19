50fa8aacd77d01e4fd5c3e73d998f4b13ecbb56e
Cai Jieying <caijieying@Cais-MacBook-Pro.local>
2019-12-20 00:00:46 +0800

    add: 在线地址

---
+++ dist/js/dataStructure.js
@@ -0,0 +1,67 @@
+export function Stack() {
+  let arr = []
+  const push = (val) => {
+    arr.push(val)
+  }
+  const pop = () => {
+    arr = arr.slice(0, arr.length - 1)
+  }
+  const top = () => {
+    if (arr.length === 0) return null
+    else return arr[arr.length - 1]
+  }
+  const empty = () => {
+    return arr.length === 0
+  }
+  return {
+    push,
+    pop,
+    top,
+    empty,
+    arr,
+  }
+}
+
+export function Deque() {
+  let arr = []
+  const push_front = (val) => {
+    arr.unshift(val)
+  }
+  const push_back = (val) => {
+    arr.push(val)
+  }
+  const pop_front = () => {
+    if (arr.length === 0) return null
+    let temp = arr[0]
+    arr = arr.slice(1)
+    return temp
+  }
+  const pop_back = () => {
+    if (arr.length === 0) return null
+    let temp = arr[arr.length - 1]
+    arr = arr.slice(0, arr.length - 1)
+    return temp
+  }
+  const front = () => {
+    if (arr.length === 0) return null
+    else return arr[0]
+  }
+  const back = () => {
+    if (arr.length === 0) return null
+    else return arr[arr.length - 1]
+  }
+  const empty = () => {
+    return arr.length === 0
+  }
+  return {
+    push_front,
+    push_back,
+    pop_front,
+    pop_back,
+    front,
+    back,
+    empty,
+  }
+}
+
+//# sourceMappingURL=dataStructure.js.map
---
+++ dist/js/index.js
@@ -0,0 +1,22 @@
+import { calcFormula } from './calculator'
+
+var submitBtn = document.getElementById('submit')
+submitBtn.addEventListener('click', function(e) {
+  const formula = document.getElementById("formula").value
+  const data = {
+    table1: {
+      field0:	20,
+      field1:	40,
+      field2:	12,
+    },
+    table2: {
+      field0:	30,
+      field1:	45,
+      field2:	7,
+    },
+  }
+
+  const result = calcFormula({formula, data})
+  document.getElementById('result').innerText = result
+})
+//# sourceMappingURL=index.js.map
